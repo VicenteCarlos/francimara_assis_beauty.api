e3d6448767531a694189364967dc782380302da7
vicentecarlos <soniamariadias2012@hotmail.com>
2025-02-04 19:50:40 -0300

    fix: use to useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const { env } = process
+const { env } = process;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  app.use(
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
